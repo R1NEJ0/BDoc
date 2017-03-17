c889bbaedf5cf4f3a0cd92a0d708b38785de316c
pench0 <negro90@gmail.com>
2017-03-18 00:40:51 +0100

    Migraciones

---
+++ database/migrations/2017_03_17_233132_CreateFileTable.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->float('size');
+            $table->string('fileName');
+            $table->string('fileExtension');
+            $table->string('name');
+            $table->integer('like');
+            $table->integer('dislike');
+            $table->string('description');
+            $table->string('keywords');
+            $table->string('url');
+            $table->softDeletes();
+
+            $table->integer('user_id')->unsigned();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
