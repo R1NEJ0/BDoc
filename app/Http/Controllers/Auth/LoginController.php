65d9d3cbc07e127b5770d531f8407f15a1342b5e
pench0 <negro90@gmail.com>
2017-03-19 23:04:12 +0100

    Auth function

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -18,6 +18,19 @@ class LoginController extends Controller
     |
     */
 
+    /*
+     *  rewrite this on authenticatesUsers to be able to login with mail or username
+      protected function credentials(Request $request)
+    {
+        $loginInput = trim($request->{$this->username()});
+        $findColumn = filter_var($loginInput, FILTER_VALIDATE_EMAIL) ? 'email' : $this->username();
+
+        return [$findColumn => $loginInput, 'password' => $request->password];
+    }
+     *
+     *
+     */
+
     use AuthenticatesUsers;
 
     /**
